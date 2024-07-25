3a16ac9676bb7633b767ad369bdfd65eaf8741be
canox82 <canox82@gmail.com>
2024-07-25 16:27:41 +0200

    primo setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://canox82.github.io',
-    base: '/bioproject',
+    base: '/sito_bioproject',
 });
