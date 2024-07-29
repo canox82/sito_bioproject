a263fb1cad901c1fc1171bf87fce00032ccc52ed
canox82 <canox82@gmail.com>
2024-07-29 16:26:57 +0200

    migliorato codice

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://canox82.github.io',
     base: 'sito_bioproject',
+    integrations: [mdx()],
 });
