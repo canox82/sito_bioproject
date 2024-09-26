c8e0ce2263c67fa574f33727e6c4eac23e7358f6
canox82 <canox82@gmail.com>
2024-09-26 17:21:24 +0200

    aggiornamenti

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://bio-project.it',
     base: '/sito_bioproject',
-    integrations: [mdx()],
+    integrations: [mdx(), sitemap()],
 });
