2249ff2a24c0b2abf2baffe8ea7776f7d8e76c0f
canox82 <canox82@gmail.com>
2024-11-07 14:27:58 +0100

    Icone e link social

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://bio-project.it',
     base: '/sito_bioproject',
-    integrations: [mdx(), sitemap()],
+    integrations: [mdx(), sitemap(), icon()],
 });
